d98c9ea2c7dd6f0bf69825a095c6bdf153f0e13d
sangmim song <awd1779@gmail.com>
2022-05-04 08:35:45 +0900

    gui forward working

--- individual/sangmim/GUI_1.m
+++ individual/sangmim/GUI_1.m
@@ -1,35 +1,35 @@
-function varargout = untitled1(varargin)
-% UNTITLED1 MATLAB code for untitled1.fig
-%      UNTITLED1, by itself, creates a new UNTITLED1 or raises the existing
+function varargout = GUI_1(varargin)
+% GUI_1 MATLAB code for GUI_1.fig
+%      GUI_1, by itself, creates a new GUI_1 or raises the existing
 %      singleton*.
 %
-%      H = UNTITLED1 returns the handle to a new UNTITLED1 or the handle to
+%      H = GUI_1 returns the handle to a new GUI_1 or the handle to
 %      the existing singleton*.
 %
-%      UNTITLED1('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UNTITLED1.M with the given input arguments.
+%      GUI_1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI_1.M with the given input arguments.
 %
-%      UNTITLED1('Property','Value',...) creates a new UNTITLED1 or raises the
+%      GUI_1('Property','Value',...) creates a new GUI_1 or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled1_OpeningFcn gets called.  An
+%      applied to the GUI before GUI_1_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to untitled1_OpeningFcn via varargin.
+%      stop.  All inputs are passed to GUI_1_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help untitled1
+% Edit the above text to modify the response to help GUI_1
 
-% Last Modified by GUIDE v2.5 04-May-2022 07:37:02
+% Last Modified by GUIDE v2.5 04-May-2022 08:25:09
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @untitled1_OpeningFcn, ...
-                   'gui_OutputFcn',  @untitled1_OutputFcn, ...
+                   'gui_OpeningFcn', @GUI_1_OpeningFcn, ...
+                   'gui_OutputFcn',  @GUI_1_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before untitled1 is made visible.
-function untitled1_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before GUI_1 is made visible.
+function GUI_1_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to untitled1 (see VARARGIN)
+% varargin   command line arguments to GUI_1 (see VARARGIN)
 
-% Choose default command line output for untitled1
+% Choose default command line output for GUI_1
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes untitled1 wait for user response (see UIRESUME)
+% UIWAIT makes GUI_1 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = untitled1_OutputFcn(hObject, eventdata, handles) 
+function varargout = GUI_1_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -94,44 +94,6 @@ function txt_theta_1_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-
-% --- Executes on button press in btn_forward.
-function btn_forward_Callback(hObject, eventdata, handles)
-% hObject    handle to btn_forward (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-Th_1 = str2double(handles.txt_theta_1.String)*pi/180;
-Th_2 = str2double(handles.txt_theta_2.String)*pi/180;
-Th_3 = str2double(handles.txt_theta_3.String)*pi/180;
-Th_4 = str2double(handles.txt_theta_4.String)*pi/180;
-Th_5 = str2double(handles.txt_theta_5.String)*pi/180;
-Th_6 = str2double(handles.txt_theta_6.String)*pi/180;
-
-L_1 = 20;
-L_2 = 50;
-L_3 = 40;
-L_4 = 40;
-L_5 = 40;
-L_6 = 40;
-
-L(1) = Link([0 L_1 0 pi/2]);
-L(2) = Link([0 0 L_2 0]);
-L(3) = Link([0 0 L_3 0]);
-L(4) = Link([0 0 L_4 0]);
-L(5) = Link([0 0 L_5 0]);
-L(6) = Link([0 0 L_6 0]);
-
-Robot = SerialLink(L);
-Robot.name = 'RRR_Robot';
-Robot.plot([Th_1 Th_2 Th_3 Th_4 Th_5 Th_6]);
-
-T = Robot.fkine([Th_1 Th_2 Th_3 Th_4 Th_5 Th_6]);
-
-handles.txt_pos_x.String = num2str(floor(T(1,4)));
-handles.txt_pos_y.String = num2str(floor(T(2,4)));
-handles.txt_pos_z.String = num2str(floor(T(3,4)));
 
 
 
@@ -318,8 +280,83 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
+% --- Executes on button press in btn_forward.
+function btn_forward_Callback(hObject, eventdata, handles)
+% hObject    handle to btn_forward (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+Th_1 = str2double(handles.txt_theta_1.String)*pi/180;
+Th_2 = str2double(handles.txt_theta_2.String)*pi/180;
+Th_3 = str2double(handles.txt_theta_3.String)*pi/180;
+Th_4 = str2double(handles.txt_theta_4.String)*pi/180;
+Th_5 = str2double(handles.txt_theta_5.String)*pi/180;
+Th_6 = str2double(handles.txt_theta_6.String)*pi/180;
+
+L_1 = 20;
+L_2 = 50;
+L_3 = 40;
+L_4 = 40;
+L_5 = 40;
+L_6 = 40;
+
+L(1) = Link([0 L_1 0 pi/2]);
+L(2) = Link([0 0 L_2 0]);
+L(3) = Link([0 0 L_3 0]);
+L(4) = Link([0 0 L_4 0]);
+L(5) = Link([0 0 L_5 0]);
+L(6) = Link([0 0 L_6 0]);
+
+Robot = SerialLink(L);
+Robot.name = 'RRR_Robot';
+Robot.plot([Th_1 Th_2 Th_3 Th_4 Th_5 Th_6]);
+
+T = Robot.fkine([Th_1 Th_2 Th_3 Th_4 Th_5 Th_6]);
+
+handles.txt_pos_x.String = num2str(floor(T(1,4)));
+handles.txt_pos_y.String = num2str(floor(T(2,4)));
+handles.txt_pos_z.String = num2str(floor(T(3,4)));
+
+
 % --- Executes on button press in btn_inverse.
 function btn_inverse_Callback(hObject, eventdata, handles)
 % hObject    handle to btn_inverse (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+PX = str2double(handles.txt_pos_x.String);
+PY = str2double(handles.txt_pos_y.String);
+PZ = str2double(handles.txt_pos_z.String);
+
+L_1 = 20;
+L_2 = 50;
+L_3 = 40;
+L_4 = 40;
+L_5 = 40;
+L_6 = 40;
+
+L(1) = Link([0 L_1 0 pi/2]);
+L(2) = Link([0 0 L_2 0]);
+L(3) = Link([0 0 L_3 0]);
+L(4) = Link([0 0 L_4 0]);
+L(5) = Link([0 0 L_5 0]);
+L(6) = Link([0 0 L_6 0]);
+
+
+Robot = SerialLink(L);
+Robot.name = 'RRR_Robot';
+
+T = [ 1 0 0 PX;
+      0 1 0 PY;
+      0 0 1 PZ;
+      0 0 0 1];
+  
+J = Robot.ikine(T, [0 0 0], [1 1 1 0 0 0]) * 180/pi;
+handles.txt_theta_1.String = num2str(floor(J(1)));
+handles.txt_theta_2.String = num2str(floor(J(2)));
+handles.txt_theta_3.String = num2str(floor(J(3)));
+handles.txt_theta_4.String = num2str(floor(J(4)));
+handles.txt_theta_5.String = num2str(floor(J(5)));
+handles.txt_theta_6.String = num2str(floor(J(6)));
+
+Robot.plot(J*pi/180);
