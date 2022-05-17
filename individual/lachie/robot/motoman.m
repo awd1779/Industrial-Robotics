b72ee38bc0968cce463b44fd9fdf5b4a82311cc3
Lachie Masters <74993848+lachlan-masters@users.noreply.github.com>
2022-05-17 18:34:20 +1000

    new robot files and updated DH

--- individual/lachie/robot/motoman.m
+++ individual/lachie/robot/motoman.m
@@ -23,27 +23,27 @@ classdef motoman < handle
         % robot = 
         self.GetMotoman();
         % robot = 
-%         self.PlotAndColourRobot();%robot,workspace);
+        self.PlotAndColourRobot();%robot,workspace);
         end
 
         %% GetUR5Robot
         % Given a name (optional), create and return a Motoman robot model
         function GetMotoman(self)
             name = "MOTOMAN HC10DTP";
-%         
-            L1 = Link('d',0.275,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            L2 = Link('d',0,'a',0.7,'alpha',-pi,'offset',-pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
-            L3 = Link('d',0,'a',0.5,'alpha',pi,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
-            L4 = Link('d',0.162,'a',0,'alpha',-pi/2,'offset',-pi/2,'qlim',[deg2rad(-360),deg2rad(360)]);
-            L5 = Link('d',0.170,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-            L6 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-
-%             L1 = Link('d',0.1,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-%             L2 = Link('d',0,'a',0.7,'alpha',0,'offset',pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
-%             L3 = Link('d',0,'a',0.1,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
-%             L4 = Link('d',0.5,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
-%             L5 = Link('d',0,'a',0.175,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+        
+%             L1 = Link('d',0.275,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+%             L2 = Link('d',0,'a',0.7,'alpha',-pi,'offset',-pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
+%             L3 = Link('d',0,'a',0.5,'alpha',pi,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+%             L4 = Link('d',0.162,'a',0,'alpha',-pi/2,'offset',-pi/2,'qlim',[deg2rad(-360),deg2rad(360)]);
+%             L5 = Link('d',0.170,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
 %             L6 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+
+            L1 = Link('d',0.275,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L2 = Link('d',0,'a',0.7,'alpha',0,'offset',pi/2,'qlim',[deg2rad(-90),deg2rad(90)]);
+            L3 = Link('d',0,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-170),deg2rad(170)]);
+            L4 = Link('d',0.5,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L5 = Link('d',0.162,'a',0,'alpha',-pi/2,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
+            L6 = Link('d',0.170,'a',0,'alpha',0,'offset',0,'qlim',[deg2rad(-360),deg2rad(360)]);
         
             self.model = SerialLink([L1 L2 L3 L4 L5 L6], 'name', name);
             self.model.delay = 0;
